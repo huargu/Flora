a83120e3657cec74c84c98e500210b0a9e49fcf2
Hüseyin Armağan Güler <h.armagan.g@gmail.com>
2018-06-24 21:36:00 +0300

    Swagger Added

--- Flora.Api/Startup.cs
+++ Flora.Api/Startup.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Flora.Api.Data;
 using Flora.Api.Database;
 using Flora.Api.Interfaces;
+using Flora.Api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,6 +17,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Flora.Api
 {
@@ -43,6 +45,10 @@ namespace Flora.Api
                     }
                 )
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Flora Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +64,11 @@ namespace Flora.Api
             }
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flora Api");
+                });
         }
     }
 }
